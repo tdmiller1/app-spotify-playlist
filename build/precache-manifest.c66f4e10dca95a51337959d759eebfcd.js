f2788fac34cf6105dafb9819937e235b60b8400d
Tucker Miller <tdmiller7@bsu.edu>
2019-10-13 12:25:41 -0400

    Fix build folder

---
+++ build/precache-manifest.c66f4e10dca95a51337959d759eebfcd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad476891033645bf1a5af9fe7dc25e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f9baa8b4df03443d28",
+    "url": "/static/js/2.b410d5b2.chunk.js"
+  },
+  {
+    "revision": "ede9eb2fa10ef52223ca",
+    "url": "/static/js/main.5f8249b3.chunk.js"
+  },
+  {
+    "revision": "d0c86602ed6b160bd13a",
+    "url": "/static/js/runtime-main.d6bfc417.js"
+  }
+]);
