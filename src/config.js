959f5e396bf28ca88ef5fa1090fb6ccd2c522f45
Tucker Miller <tdmiller7@bsu.edu>
2019-10-13 19:53:54 -0400

    build

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'development'){
     var config = {
-        authApiUrl: 'https://api-tuckermillerdev.herokuapp.com/',
-        url: 'https://app-spotify-app.herokuapp.com/'
+        authApiUrl: 'https://api-tuckermillerdev.herokuapp.com',
+        url: 'https://app-spotify-app.herokuapp.com'
     }
 }else{
     config = {
